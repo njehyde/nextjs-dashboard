3179e5397da1cfde5d0bf571f01f040ecbce7cf3
Nick Hyde <nick.hyde@immediate.co.uk>
2023-11-24 08:26:03 +0000

    feat: add finished course files

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -28,10 +28,10 @@ export default async function InvoicesTable({
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
-                        alt="invoices image"
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
